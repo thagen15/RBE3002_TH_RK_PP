632803ec186179ce82b24121adf1ad70b5e8f627
ppatias <ppatias@wpi.edu>
2017-04-01 17:08:34 -0400

    Add files via upload
    lab3_sign-off[1-3]

---
+++ ppatias_lab3_m.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import rospy, tf, numpy, math
+from nav_msgs.msg import GridCells
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Point, Pose, PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry, OccupancyGrid
+#from kobuki_msgs.msg import BumperEvent
+
+# reads in global map
+def mapCallBack(data):
+    global mapData
+    global width
+    global height
+    global mapgrid
+    global resolution
+    global offsetX
+    global offsetY
+
+    mapgrid = data
+    resolution = data.info.resolution
+    mapData = data.data
+    width = data.info.width
+    height = data.info.height
+    offsetX = data.info.origin.position.x
+    offsetY = data.info.origin.position.y
+    #print data.info
+
+
+def publishCells(grid):
+    global pub
+    print "publishing"
+
+    # resolution and offset of the map
+    k=0
+    cells = GridCells()
+    cells.header.frame_id = 'map'
+    cells.cell_width = resolution 
+    cells.cell_height = resolution
+
+    
+    for i in range(1,height): #height should be set to hieght of grid
+        k=k+1
+        for j in range(1,width): #width should be set to width of grid
+            k=k+1
+            #print k # used for debugging
+            if (grid[k] == 100):
+                point=Point()
+                point.x=(j*resolution)+offsetX + (1.5 * resolution) # added secondary offset 
+                point.y=(i*resolution)+offsetY - (.5 * resolution) # added secondary offset ... Magic ?
+                point.z=0
+                cells.cells.append(point)
+    pub.publish(cells)           
+
+#Main handler of the project
+def run():
+    global pub
+    rospy.init_node('ppatias_lab3')
+    sub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
+    pub = rospy.Publisher("/ourGrid", GridCells, queue_size=1)  
+   # pubpath = rospy.Publisher("/path", GridCells, queue_size=1) # you can use other types if desired
+   # pubway = rospy.Publisher("/waypoints", GridCells, queue_size=1)
+    #goal_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, readGoal, queue_size=1) #change topic for best results
+    #goal_sub = rospy.Subscriber('initialpose', PoseWithCovarianceStamped, readStart, queue_size=1) #change topic for best results
+
+    # wait a second for publisher, subscribers, and TF
+    rospy.sleep(1)
+
+
+
+    while (1 and not rospy.is_shutdown()):
+        publishCells(mapData) #publishing map data every 2 seconds
+        rospy.sleep(2)  
+        print("Complete")
+    
+
+
+if __name__ == '__main__':
+    try:
+        run()
+    except rospy.ROSInterruptException:
+        pass
